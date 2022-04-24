2b54c321d5178951cd4290937f690bca13649cb8
unknown <aayan.work99@gmail.com>
2022-04-25 01:31:00 +0530

    linkedin link added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-            <a className="flex justify-between items-center w-full text-gray-300" href="/">
+            <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/aayanjit-bhuyan-b48705195/" target='_blank' rel="noreferrer">
               Linkedin <FaLinkedin size={30}/>
             </a>
           </li>
